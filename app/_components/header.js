ee708cb384004a16851de57447b13d2e24bb52f4
Professor Cosme - SENAC <cosme.teixeira@gmail.com>
2025-01-28 11:03:48 -0300

    atualizando

--- app/_components/header.js
+++ app/_components/header.js
@@ -5,7 +5,7 @@ export default function Header() {
         <div className='flex items-center justify-between px-2'>
             <div className='flex flex-col'>
             <span className='text-black font-extrabold py-2 text-2xl'>Dev Motors</span>
-            <span className='text-black  py-2 text-sm'>Prof. Cosme Teixeira Silva Jan/2025</span>
+            <span className='text-black  py-2 text-sm text-[#121212] opacity-60'>Prof. Cosme Teixeira Silva - Jan/2025</span>
             </div>
            
             <nav className='text-gray-600 flex space-x-8 text-[0.6rem] sm:text-sm'>
